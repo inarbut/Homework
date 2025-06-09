17eb72e60e560c04d430143593c4f4fb0e4b695f
inarbut <inarbut@kse.org.ua>
2025-06-09 22:04:12 +0300

    added a new function to randWords and te ability to stop when the length is incorrect

--- randWords.py
+++ randWords.py
@@ -3,3 +3,20 @@ import random as r
 def give_random_word():
     return r.choice(['apple','bread','candy','dream','eagle','flame','grape','house','input','joker'])
 
+def give_guesses_output(word, guess):
+    wordLength = len(word)
+    display = ['']*wordLength#to handle the priority, and I think the list coprehension here looks pretty clean
+    temp_dict = {k: word.count(k) for k in set(word)} #uhhh, yeah, I think it looks much cleaner than writing it all in sperarate lines. It is used so it marks the correct amount of letters
+    for k in range(wordLength):# we have 2 separate loops as in wordle the first prioriy should always be - correct index and character
+        if guess[k] == word[k]: #specific index
+            display[k]=(f"[{guess[k]}]")# replaced the append wih fixed change, as the append just add in the end
+            temp_dict[guess[k]]-=1 #if the letter in the guess is in the secret_word, make the amount of apperences of that letter in the secret_word smaller by one
+
+    for k in range(wordLength):
+        if display[k] == '':
+            if guess[k] in word and temp_dict[guess[k]] != 0: #check if this character was already added to display
+                display[k]=(f"({guess[k]})")
+                temp_dict[guess[k]]-=1
+            else:
+                display[k]=(guess[k])
+    return display
--- wordle.py
+++ wordle.py
@@ -24,7 +24,7 @@ while True: #added a total while loop
         while len(guess)!=secret_word_length: #added a while loop so that it doesnt decrease tries, because if we have a for loop and continue, it would decrease tries
             print("Wrong length. Expected the length of", secret_word_length)
             guess = input(f"Attempt {i+1}/6 – Enter guess: ").lower()
-            if guess == "stop": #added it here in case a user wants to quit after he inputted wrong length
+            if guess == "stop": #added it here in case a user wants to quit after he inputed wrong length
                 totalBreak = True
                 break
         
@@ -35,20 +35,7 @@ while True: #added a total while loop
             break
 
         
-        display = ['']*secret_word_length#to handle the priority, and I think the list coprehension here looks pretty clean
-        temp_dict = {k: secret_word.count(k) for k in set(secret_word)} #uhhh, yeah, I think it looks much cleaner than writing it all in sperarate lines. It is used so it marks the correct amount of letters
-        for k in range(secret_word_length):# we have 2 separate loops as in wordle the first prioriy should always be - correct index and character
-            if guess[k] == secret_word[k]: #specific index
-                display[k]=(f"[{guess[k]}]")# replaced the append wih fixed change, as the append just add in the end
-                temp_dict[guess[k]]-=1 #if the letter in the guess is in the secret_word, make the amount of apperences of that letter in the secret_word smaller by one
-
-        for k in range(secret_word_length):
-            if display[k] == '':
-                if guess[k] in secret_word and temp_dict[guess[k]] != 0: #check if this character was already added to display
-                    display[k]=(f"({guess[k]})")
-                    temp_dict[guess[k]]-=1
-                else:
-                    display[k]=(guess[k])
+        display = randWords.give_guesses_output(secret_word, guess)
 
 
         #removed junk
