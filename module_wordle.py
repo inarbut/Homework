6ede2d8407bfc823fd69c43f4ddb23368f1c5591
inarbut <inarbut@kse.org.ua>
2025-06-14 15:00:53 +0300

    added a main function for playing

--- module_wordle.py
+++ module_wordle.py
@@ -5,18 +5,26 @@ def give_random_word():
 
 def give_guesses_output(word, guess):
     wordLength = len(word)
-    display = ['']*wordLength#to handle the priority, and I think the list coprehension here looks pretty clean
+    display = ['']*wordLength # creating a display which we will populate with characters and their parameters (correct/not)
     temp_dict = {k: word.count(k) for k in set(word)} #uhhh, yeah, I think it looks much cleaner than writing it all in sperarate lines. It is used so it marks the correct amount of letters
     for k in range(wordLength):# we have 2 separate loops as in wordle the first prioriy should always be - correct index and character
         if guess[k] == word[k]: #specific index
-            display[k]=(f"[{guess[k]}]")# replaced the append wih fixed change, as the append just add in the end
+            display[k]=(f"[{guess[k]}]")# replaced the append wih fixed change, as the append just adds in the end
             temp_dict[guess[k]]-=1 #if the letter in the guess is in the secret_word, make the amount of apperences of that letter in the secret_word smaller by one
 
     for k in range(wordLength):
-        if display[k] == '':
+        if display[k] == '': #check if it hasnt been populated yet
             if guess[k] in word and temp_dict[guess[k]] != 0: #check if this character was already added to display
                 display[k]=(f"({guess[k]})")
                 temp_dict[guess[k]]-=1
             else:
                 display[k]=(guess[k])
     return display
+
+def get_correct_guess(guess, len):
+    while len(guess)!=len: #added a while loop so that it doesnt decrease tries, because if we have a for loop and continue, it would decrease tries
+        print("Wrong length. Expected the length of", len)
+        guess = input(f"Attempt {i+1}/6 – Enter guess: ").lower()
+        if guess == "stop": #added it here in case a user wants to quit after he inputed wrong length
+            return "stop"
+    return guess
--- wordle.py
+++ wordle.py
@@ -1,10 +1,10 @@
-import randWords #added an import
+import module_wordle #added an import
 
 print("Welcome to Wordle!") #moved it to the top to not to be printed every time
 
-while True: #added a total while loop
 
-    secret_word = randWords.give_random_word().lower() #called a function to get a random word from that module and also added .lower() in case the word is uppercase
+def play():
+    secret_word = module_wordle.give_random_word().lower() #called a function to get a random word from that module and also added .lower() in case the word is uppercase
     #removed x, y, z as there was no need for them
 
     tries = 6
@@ -13,34 +13,27 @@ while True: #added a total while loop
     print("Guess the",secret_word_length,"-letter word. You have", tries, "tries. Type stop to stop")
 
     won = False #variable to print if you won or no
-    totalBreak = False #variable for global stop
     for i in range(tries): #replaced a while loop with a for loop
         guess = input(f"Attempt {i+1}/6 – Enter guess: ").lower() #replaced tries to i, to handle any amount of tries
 
+        guess = module_wordle.get_correct_guess(guess, secret_word_length, i+1)
+
         if guess == "stop": #added a global break to stop the entire program without keyboardInterruption
-            totalBreak = True
-            break
-        
-        while len(guess)!=secret_word_length: #added a while loop so that it doesnt decrease tries, because if we have a for loop and continue, it would decrease tries
-            print("Wrong length. Expected the length of", secret_word_length)
-            guess = input(f"Attempt {i+1}/6 – Enter guess: ").lower()
-            if guess == "stop": #added it here in case a user wants to quit after he inputed wrong length
-                totalBreak = True
-                break
+            return False
         
 
         if guess==secret_word:
             print("You won!!!")
-            won = True
-            break
+            return True
 
         
-        display = randWords.give_guesses_output(secret_word, guess)
+        display = module_wordle.give_guesses_output(secret_word, guess)
 
 
         #removed junk
         print("Result:", *display) #removed list comprehension as it already a list and no need for modification, replaced .join with * for clearer output 
-    if totalBreak == True:
+    print("You lose! The word was:", secret_word)
+
+while True:
+    if not play():
         break
-    elif won == False: #elif here, because if you dont put it, it will print that you won and what the secret word was after you stop
-        print("You lose! The word was:", secret_word)
